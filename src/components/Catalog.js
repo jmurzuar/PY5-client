3ca5716166caef57582e1977e96205116a3e115d
jmurzuar <jmurzuar@gmail>
2024-05-04 12:22:29 -0400

    first commit

---
+++ src/components/Catalog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListProducts from './ListProducts'
+
+export default function Catalog() {
+    return (
+        <div>
+            <ListProducts title="Todas las guitarras" />
+        </div>
+    )
+}
